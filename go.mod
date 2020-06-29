09c1b8dd73c59b442c0f3714c9df42db7ed5cb0d
David Gamba <davidgamba@gmail.com>
2020-06-28 21:27:17 -0600

    Pull antlr updates

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.13
 
 require (
 	github.com/DavidGamba/go-getoptions v0.17.0
-	github.com/antlr/antlr4 v0.0.0-20200119161855-7a3f40bc341d
+	github.com/antlr/antlr4 v0.0.0-20200622152914-7a6db6a50abf
 )
--- main.go
+++ main.go
@@ -6,6 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+//go:generate ./antlr4 -Xexact-output-dir -o parser -visitor -Dlanguage=Go ./LabeledExpr.g4
 package main
 
 import (
